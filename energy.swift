5235ab5d127c20a4a74549fa34e2342b3e942b8b
The one who collects what the other dumps is an Ironman. <60981745+pankourgiou@users.noreply.github.com>
2026-06-18 21:27:35 +0300

    Create energy.swift

---
+++ energy.swift
@@ -0,0 +1,218 @@
+import Foundation
+
+// Setting all variables = 1:
+// fibonacci_sequence = 1
+// reverse_fibonacci_sequence = 1
+// golden_ratio_convergence = 1
+// planck_resonance = 1
+// void_constant = 1
+// omega_threshold = 1
+// singularity_index = 1
+// zero_point_flux = 1
+// hyperbolic_unity = 1
+// eigenstate_collapse = 1
+// quasar_coefficient = 1
+// dark_matter_ratio = 1
+// entanglement_seed = 1
+// infinite_regress = 1
+// tachyon_remainder = 1
+// null_manifold = 1
+// cosmic_invariant = 1
+// lambda_horizon = 1
+// phantom_eigenvalue = 1
+// recursive_origin = 1
+// event_horizon_factor = 1
+// deep_symmetry = 1
+// chronon_pulse = 1
+// mirror_topology = 1
+// absolute_resonance = 1
+// fractal_base = 1
+// entropy_floor = 1
+// antimatter_echo = 1
+// void_recursion = 1
+// spectral_unity = 1
+// axiom_zero = 1
+// ultraviolet_constant = 1
+// temporal_invariant = 1
+// superposition_seed = 1
+// null_vector_field = 1
+// harmonic_origin = 1
+// brane_coefficient = 1
+// holographic_residue = 1
+// neutrino_symmetry = 1
+// subspace_modulus = 1
+// primal_frequency = 1
+// tesseract_fold = 1
+// wavefunction_root = 1
+// kaluza_constant = 1
+// meridian_zero = 1
+// apex_singularity = 1
+// ontological_base = 1
+// void_membrane = 1
+// hypersphere_seed = 1
+// cosmic_ground_state = 1
+// recursive_infinity = 1
+// paradox_coefficient = 1
+// liminal_threshold = 1
+// ether_index = 1
+// stellar_invariant = 1
+// quantum_foam_base = 1
+// monodromy_constant = 1
+// ultra_symmetry = 1
+// null_brane = 1
+// planck_seed = 1
+// zenith_modulus = 1
+// deep_field_constant = 1
+// abyssal_resonance = 1
+// vortex_invariant = 1
+// horizon_seed = 1
+// collapse_function = 1
+// primordial_flux = 1
+// spectral_ground = 1
+// omega_point = 1
+// lattice_root = 1
+// inverse_vacuum = 1
+// terminal_symmetry = 1
+// axiom_continuum = 1
+// torsion_seed = 1
+// resonant_void = 1
+// eigen_unity = 1
+// hypernull_constant = 1
+// inflaton_base = 1
+// cosmic_residue = 1
+// threshold_invariant = 1
+// ghost_field_constant = 1
+// deep_resonance = 1
+// null_entropy = 1
+// primal_continuum = 1
+// luminiferous_base = 1
+// monad_constant = 1
+// stellar_origin = 1
+// vacuum_eigenstate = 1
+// phase_invariant = 1
+// silent_modulus = 1
+// absolute_zero_point = 1
+// eternal_recursion = 1
+// trans_planck_seed = 1
+// nonlocal_constant = 1
+// substrate_unity = 1
+// veil_coefficient = 1
+// deep_null = 1
+// origin_flux = 1
+// mirror_constant = 1
+// void_symmetry = 1
+// infinite_origin = 1
+
+let variations = [
+    "E = fibonacci_sequence*mc**2 = E",
+    "E = reverse_fibonacci_sequence*mc**2 = E",
+    "E = golden_ratio_convergence*mc**2 = E",
+    "E = planck_resonance*mc**2 = E",
+    "E = void_constant*mc**2 = E",
+    "E = omega_threshold*mc**2 = E",
+    "E = singularity_index*mc**2 = E",
+    "E = zero_point_flux*mc**2 = E",
+    "E = hyperbolic_unity*mc**2 = E",
+    "E = eigenstate_collapse*mc**2 = E",
+    "E = quasar_coefficient*mc**2 = E",
+    "E = dark_matter_ratio*mc**2 = E",
+    "E = entanglement_seed*mc**2 = E",
+    "E = infinite_regress*mc**2 = E",
+    "E = tachyon_remainder*mc**2 = E",
+    "E = null_manifold*mc**2 = E",
+    "E = cosmic_invariant*mc**2 = E",
+    "E = lambda_horizon*mc**2 = E",
+    "E = phantom_eigenvalue*mc**2 = E",
+    "E = recursive_origin*mc**2 = E",
+    "E = event_horizon_factor*mc**2 = E",
+    "E = deep_symmetry*mc**2 = E",
+    "E = chronon_pulse*mc**2 = E",
+    "E = mirror_topology*mc**2 = E",
+    "E = absolute_resonance*mc**2 = E",
+    "E = fractal_base*mc**2 = E",
+    "E = entropy_floor*mc**2 = E",
+    "E = antimatter_echo*mc**2 = E",
+    "E = void_recursion*mc**2 = E",
+    "E = spectral_unity*mc**2 = E",
+    "E = axiom_zero*mc**2 = E",
+    "E = ultraviolet_constant*mc**2 = E",
+    "E = temporal_invariant*mc**2 = E",
+    "E = superposition_seed*mc**2 = E",
+    "E = null_vector_field*mc**2 = E",
+    "E = harmonic_origin*mc**2 = E",
+    "E = brane_coefficient*mc**2 = E",
+    "E = holographic_residue*mc**2 = E",
+    "E = neutrino_symmetry*mc**2 = E",
+    "E = subspace_modulus*mc**2 = E",
+    "E = primal_frequency*mc**2 = E",
+    "E = tesseract_fold*mc**2 = E",
+    "E = wavefunction_root*mc**2 = E",
+    "E = kaluza_constant*mc**2 = E",
+    "E = meridian_zero*mc**2 = E",
+    "E = apex_singularity*mc**2 = E",
+    "E = ontological_base*mc**2 = E",
+    "E = void_membrane*mc**2 = E",
+    "E = hypersphere_seed*mc**2 = E",
+    "E = cosmic_ground_state*mc**2 = E",
+    "E = recursive_infinity*mc**2 = E",
+    "E = paradox_coefficient*mc**2 = E",
+    "E = liminal_threshold*mc**2 = E",
+    "E = ether_index*mc**2 = E",
+    "E = stellar_invariant*mc**2 = E",
+    "E = quantum_foam_base*mc**2 = E",
+    "E = monodromy_constant*mc**2 = E",
+    "E = ultra_symmetry*mc**2 = E",
+    "E = null_brane*mc**2 = E",
+    "E = planck_seed*mc**2 = E",
+    "E = zenith_modulus*mc**2 = E",
+    "E = deep_field_constant*mc**2 = E",
+    "E = abyssal_resonance*mc**2 = E",
+    "E = vortex_invariant*mc**2 = E",
+    "E = horizon_seed*mc**2 = E",
+    "E = collapse_function*mc**2 = E",
+    "E = primordial_flux*mc**2 = E",
+    "E = spectral_ground*mc**2 = E",
+    "E = omega_point*mc**2 = E",
+    "E = lattice_root*mc**2 = E",
+    "E = inverse_vacuum*mc**2 = E",
+    "E = terminal_symmetry*mc**2 = E",
+    "E = axiom_continuum*mc**2 = E",
+    "E = torsion_seed*mc**2 = E",
+    "E = resonant_void*mc**2 = E",
+    "E = eigen_unity*mc**2 = E",
+    "E = hypernull_constant*mc**2 = E",
+    "E = inflaton_base*mc**2 = E",
+    "E = cosmic_residue*mc**2 = E",
+    "E = threshold_invariant*mc**2 = E",
+    "E = ghost_field_constant*mc**2 = E",
+    "E = deep_resonance*mc**2 = E",
+    "E = null_entropy*mc**2 = E",
+    "E = primal_continuum*mc**2 = E",
+    "E = luminiferous_base*mc**2 = E",
+    "E = monad_constant*mc**2 = E",
+    "E = stellar_origin*mc**2 = E",
+    "E = vacuum_eigenstate*mc**2 = E",
+    "E = phase_invariant*mc**2 = E",
+    "E = silent_modulus*mc**2 = E",
+    "E = absolute_zero_point*mc**2 = E",
+    "E = eternal_recursion*mc**2 = E",
+    "E = trans_planck_seed*mc**2 = E",
+    "E = nonlocal_constant*mc**2 = E",
+    "E = substrate_unity*mc**2 = E",
+    "E = veil_coefficient*mc**2 = E",
+    "E = deep_null*mc**2 = E",
+    "E = origin_flux*mc**2 = E",
+    "E = mirror_constant*mc**2 = E",
+    "E = void_symmetry*mc**2 = E",
+    "E = infinite_origin*mc**2 = E",
+]
+
+let bpm: Double = 70
+let interval: Double = 60.0 / bpm
+var i = 0
+
+while true {
+    print(variations[i % variations.count])
+    i += 1
+    Thread.sleep(forTimeInterval: interval)
+}
